73dc1817f29c1c5de127aaf940c801ffab912a7b
Dan Corder <dev@dancorder.com>
2013-11-01 22:07:15 +0000

    More file tree walker work

--- src/main/java/com/dancorder/PhotoSync/ParallelFileTreeWalker/ParallelFileTreeWalker.java
+++ src/main/java/com/dancorder/PhotoSync/ParallelFileTreeWalker/ParallelFileTreeWalker.java
@@ -1,13 +1,19 @@
 package com.dancorder.PhotoSync.ParallelFileTreeWalker;
 
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.io.IOException;
 
 public class ParallelFileTreeWalker {
 	
-	private Path root1;
-	private Path root2;
-	private IParallelFileTreeVisitor visitor;
+	private final Path root1;
+	private final Path root2;
+	private final IParallelFileTreeVisitor visitor;
 
 	public ParallelFileTreeWalker(Path path1, Path path2, IParallelFileTreeVisitor visitor) {
 		root1 = path1;
@@ -15,7 +21,34 @@ public class ParallelFileTreeWalker {
 		this.visitor = visitor;
 	}
 	
-	public void walk() {
+	public void walk() throws IOException {
 		visitor.preVisitDirectory(Paths.get(""), FileExistence.BothPaths);
+		
+		List<Path> path1Files = getSortedFiles(root1);
+		List<Path> path2Files = getSortedFiles(root2);
+		
+		
+		if (path1Files.size() > 0 || path2Files.size() > 0) {
+		
+			Path relativeFilePath1 = root1.relativize(path1Files.get(0));
+			Path relativeFilePath2 = root2.relativize(path2Files.get(0));
+			
+			if (relativeFilePath1.equals(relativeFilePath2)) {
+				visitor.visitFile(relativeFilePath1, FileExistence.BothPaths);
+			}
+		}
+	}
+	
+	private List<Path> getSortedFiles(Path directory) throws IOException {
+		List<Path> ret = new ArrayList<Path>();
+		try (DirectoryStream<Path> ds = Files.newDirectoryStream(directory)) {
+			for (Path p : ds) {
+				ret.add(p); 
+			}
+		}
+		
+		Collections.sort(ret);
+		
+		return ret;
 	}
 }
--- src/test/java/com/dancorder/PhotoSync/ParallelFileTreeWalker/ParallelFileTreeWalkerTest.java
+++ src/test/java/com/dancorder/PhotoSync/ParallelFileTreeWalker/ParallelFileTreeWalkerTest.java
@@ -2,6 +2,8 @@ package com.dancorder.PhotoSync.ParallelFileTreeWalker;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -34,12 +36,59 @@ public class ParallelFileTreeWalkerTest {
 			
 		}
 		finally {
-			Files.delete(tempRootPath1);
-			Files.delete(tempRootPath2);
+			cleanUpDirectory(tempRootPath1);
+			cleanUpDirectory(tempRootPath2);
+		}
+	}
+	
+	@Test
+	public void testSingleFileBothPathsRoot() throws IOException {
+		Path tempRootPath1 = null;
+		Path tempRootPath2 = null;
+		
+		try {
+			tempRootPath1 = createRootDirectory();
+			tempRootPath2 = createRootDirectory();
+			Path filePath1 = createFile(tempRootPath1, "testFile");
+			createFile(tempRootPath2, filePath1.getFileName().toString());
+			TestParallelFileTreeVisitor tpftv = new TestParallelFileTreeVisitor();
+			
+			ParallelFileTreeWalker pftw = new ParallelFileTreeWalker(tempRootPath1, tempRootPath2, tpftv);
+			pftw.walk();
+			
+			List<MethodCall> expected = new ArrayList<MethodCall>();
+			expected.add(new MethodCallPreVisitDirectory(Paths.get(""), FileExistence.BothPaths));
+			expected.add(new MethodCallVisitFile(filePath1.getFileName(), FileExistence.BothPaths));
+			assertEquals(expected, tpftv.getCalls());
+		}
+		finally {
+			cleanUpDirectory(tempRootPath1);
+			cleanUpDirectory(tempRootPath2);
 		}
 	}
 	
 	private Path createRootDirectory() throws IOException {
 		return Files.createTempDirectory(null);
 	}
+	
+	private Path createFile(Path directory, String fileName) throws IOException {
+		return Files.createFile(directory.resolve(fileName));
+	}
+	
+	private void cleanUpDirectory(Path directory) throws IOException {
+		if (!deleteRecursive(directory.toFile())) {
+			throw new IOException("Failed to clean up directory " + directory.toString());
+		}
+	}
+	
+	private boolean deleteRecursive(File path) throws FileNotFoundException{
+        if (!path.exists()) throw new FileNotFoundException(path.getAbsolutePath());
+        boolean ret = true;
+        if (path.isDirectory()) {
+            for (File f : path.listFiles()) {
+                ret = ret && deleteRecursive(f);
+            }
+        }
+        return ret && path.delete();
+    }
 }
