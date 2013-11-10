97b3238fa21d7b3b96f8be13ecccd3f85f0218d7
Dan Corder <dev@dancorder.com>
2013-11-10 22:51:00 +0000

    Update main method

--- src/main/java/com/dancorder/PhotoSync/PhotoSync.java
+++ src/main/java/com/dancorder/PhotoSync/PhotoSync.java
@@ -3,6 +3,11 @@
  */
 package com.dancorder.PhotoSync;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.dancorder.PhotoSync.ParallelFileTreeWalker.ParallelFileTreeWalker;
+
 /**
  * @author Dan
  *
@@ -12,10 +17,21 @@ public class PhotoSync {
 	/**
 	 * @param args
 	 * @throws UsageException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws UsageException {
+	public static void main(String[] args) throws UsageException, IOException {
 		Parameters params = new Parameters(args);
 
+		SynchingVisitor visitor = new SynchingVisitor(params.getPath1(), params.getPath2());
+		ParallelFileTreeWalker walker = new ParallelFileTreeWalker(params.getPath1(), params.getPath2(), visitor);
+		
+		walker.walk();
+		
+		List<Action> actions = visitor.getActions();
+		
+		for (Action action :actions) {
+			System.out.println(action.toString());
+		}
 	}
 
 }
--- src/test/java/com/dancorder/PhotoSync/PhotoSyncTest.java
+++ src/test/java/com/dancorder/PhotoSync/PhotoSyncTest.java
@@ -1,5 +1,7 @@
 package com.dancorder.PhotoSync;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import com.dancorder.PhotoSync.PhotoSync;
@@ -8,12 +10,12 @@ import com.dancorder.PhotoSync.UsageException;
 public class PhotoSyncTest {
 
 	@Test(expected = UsageException.class)
-	public void testNullParameters() throws UsageException {
+	public void testNullParameters() throws UsageException, IOException {
 		PhotoSync.main(null);
 	}
 	
 	@Test(expected = UsageException.class)
-	public void testNoParameters() throws UsageException {
+	public void testNoParameters() throws UsageException, IOException {
 		PhotoSync.main(new String[0]);
 	}
 
