17f784d00e8f4ddf41dac479be0a8f79b6021e62
Dan Corder <dev@dancorder.com>
2013-11-11 22:02:45 +0000

    Naughty unTDD main method

--- src/main/java/com/dancorder/PhotoSync/PhotoSync.java
+++ src/main/java/com/dancorder/PhotoSync/PhotoSync.java
@@ -5,6 +5,7 @@ package com.dancorder.PhotoSync;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 import com.dancorder.PhotoSync.ParallelFileTreeWalker.ParallelFileTreeWalker;
 
@@ -20,6 +21,7 @@ public class PhotoSync {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws UsageException, IOException {
+		//TODO: Test/refactor this method
 		Parameters params = new Parameters(args);
 
 		SynchingVisitor visitor = new SynchingVisitor(params.getPath1(), params.getPath2());
@@ -29,8 +31,24 @@ public class PhotoSync {
 		
 		List<Action> actions = visitor.getActions();
 		
+		System.out.println("Actions found:");
 		for (Action action :actions) {
 			System.out.println(action.toString());
+		}
+		
+		System.out.println("Execute actions (y/n)?");
+		String response;
+		try (Scanner scanIn = new Scanner(System.in)) {
+			do {
+			    response = scanIn.nextLine();
+			} while (!response.equals("y") && !response.equals("n"));
+		}
+		
+		if (response.equals("y")) {
+			for (Action action :actions) {
+				System.out.println("Executing: " + action.toString());
+				action.doAction();
+			}
 		}
 	}
 
