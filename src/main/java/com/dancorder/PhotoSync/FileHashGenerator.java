979ad124f9eafb6ce002acb43bdd4443997f4fd7
Dan Corder <dev@dancorder.com>
2014-02-17 21:52:52 +0000

    Change HashGenerator to FileHashGenerator and change method to take a Path rather than a stream

---
+++ src/main/java/com/dancorder/PhotoSync/FileHashGenerator.java
@@ -0,0 +1,22 @@
+package com.dancorder.PhotoSync;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+class FileHashGenerator {
+	String calculateMd5(Path file) throws IOException {
+		FileInputStream stream = null;
+		try {
+			stream = new FileInputStream(file.toString());
+			return DigestUtils.md5Hex(stream);
+		}
+		finally {
+			if (stream != null) {
+				stream.close();
+			}
+		}
+	}
+}
