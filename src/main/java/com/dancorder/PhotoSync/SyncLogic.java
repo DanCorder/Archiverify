d5be96db9ec2cad2175e774906659e01b6b2dda6
Dan Corder <dev@dancorder.com>
2014-03-11 18:52:47 +0000

    Refactor sync logic

--- src/main/java/com/dancorder/PhotoSync/SyncLogic.java
+++ src/main/java/com/dancorder/PhotoSync/SyncLogic.java
@@ -16,54 +16,59 @@ class SyncLogic {
 	}
 	
 	List<Action> compareFiles(Path file1, FileHashStore store1, Path file2, FileHashStore store2, FileExistence existence) throws IOException {
-		String hashFromStore1 = store1.getHash(file1.getFileName());
-		String hashFromStore2 = store2.getHash(file2.getFileName());
 		String hashFromFile1 = hashGenerator.calculateMd5(file1);
 		String hashFromFile2 = hashGenerator.calculateMd5(file2);
+		String hashFromStore1 = store1.getHash(file1.getFileName());
+		String hashFromStore2 = store2.getHash(file2.getFileName());
+
+		if (allHashesMatch(hashFromFile1, hashFromStore1, hashFromFile2, hashFromStore2)) {
+			return new ArrayList<Action>();
+		}
+		
+		if (hashFromFile1 == hashFromStore1) {
+			return oneFileAndStoreHashMatch(hashFromFile1, hashFromStore1, hashFromFile2, hashFromStore2, file1, file2, store1, store2);
+		}
+		
+		if (hashFromFile2 == hashFromStore2) {
+			return oneFileAndStoreHashMatch(hashFromFile2, hashFromStore2, hashFromFile1, hashFromStore1, file2, file1, store2, store1);
+		}
+
+		return null;
+	}
+	
+	private List<Action> oneFileAndStoreHashMatch(
+			String matchingHashFromFile,
+			String matchingHashFromStore,
+			String otherHashFromFile,
+			String otherHashFromStore,
+			Path matchingFile,
+			Path otherFile,
+			FileHashStore matchingStore,
+			FileHashStore otherStore) {
+		
 		ArrayList<Action> actions = new ArrayList<Action>();
 		
-		if (hashFromStore1 != hashFromStore2) {
-			if (hashFromFile1 == hashFromFile2) {
-				if (hashFromStore1 == hashFromFile1) {
-					store2.setHash(file2.getFileName(), hashFromStore1);
-					return actions;
-				}
-				if (hashFromStore2 == hashFromFile1) {
-					store1.setHash(file1.getFileName(), hashFromStore2);
-					return actions;
-				}
-			}
-			else if (hashFromFile1 == hashFromStore1 && hashFromFile2 == hashFromStore2) {
-				actions.add(new SyncWarningAction("File " + file1 + " and file " + file2 + " are different but both have matching hashes. Please manually move or delete the incorrect file."));
-				return actions;
-			}
-			else if (hashFromFile1 == hashFromStore1 && hashFromFile2 != hashFromStore2) {
-				actions.add(new FileCopyAction(file1, file2));
-				store2.setHash(file2.getFileName(), hashFromStore1);
-				return actions;
-			}
-			else if (hashFromFile1 != hashFromStore1 && hashFromFile2 == hashFromStore2) {
-				actions.add(new FileCopyAction(file2, file1));
-				store1.setHash(file1.getFileName(), hashFromStore2);
-				return actions;
-			}
+		if (otherHashFromFile == otherHashFromStore) {
+			actions.add(new SyncWarningAction("File " + matchingFile + " and file " + otherFile + " are different but both have matching hashes. Please manually move or delete the incorrect file."));
 		}
-		else if (hashFromFile1 != hashFromFile2) {
-			if (hashFromStore1 == hashFromStore2) {
-				if (hashFromFile1 == hashFromStore1) {
-					actions.add(new FileCopyAction(file1, file2));
-					return actions;
-				}
-				if (hashFromFile2 == hashFromStore1) {
-					actions.add(new FileCopyAction(file2, file1));
-					return actions;
-				}
-			}
+		else if (matchingHashFromFile == otherHashFromFile) {
+			otherStore.setHash(otherFile.getFileName(), otherHashFromFile);
+		}
+		else if (matchingHashFromStore == otherHashFromStore) {
+			actions.add(new FileCopyAction(matchingFile, otherFile));
+		}
+		else {
+			otherStore.setHash(otherFile.getFileName(), matchingHashFromStore);
+			actions.add(new FileCopyAction(matchingFile, otherFile));
 		}
 
 		return actions;
 	}
-	
+
+	private boolean allHashesMatch(String hash1, String hash2,String hash3, String hash4) {
+		return hash1 == hash2 && hash2 == hash3 && hash3 == hash4;
+	}
+
 	List<Action> compareDirectories(Path absolutePath1, Path absolutePath2, FileExistence existence) {
 		return new ArrayList<Action>();
 	}
