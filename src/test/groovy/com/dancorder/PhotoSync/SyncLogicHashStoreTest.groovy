76838793b6019b98a240ac50374327ae61309e0a
Dan Corder <dev@dancorder.com>
2014-05-25 21:37:34 +0100

    Refactor sync logic hash store tests

--- src/test/groovy/com/dancorder/PhotoSync/SyncLogicHashStoreTest.groovy
+++ src/test/groovy/com/dancorder/PhotoSync/SyncLogicHashStoreTest.groovy
@@ -6,14 +6,12 @@ class SyncLogicHashStoreTest extends spock.lang.Specification {
 //	The rules for the hash store synching logic are simple:
 //    - If a hash store has some changes then write it out
 //    - Otherwise do nothing
-
+	
 	def "hash stores unchanged"() {
 		setup:
 		def logic = new SyncLogic(null)
-		def store1 = Mock(FileHashStore)
-		def store2 = Mock(FileHashStore)
-		store1.isDirty() >> false;
-		store2.isDirty() >> false;
+		def store1 = createMockStore(false)
+		def store2 = createMockStore(false)
 		def expectedResult = new ArrayList<Action>()
 		
 		when:
@@ -26,13 +24,9 @@ class SyncLogicHashStoreTest extends spock.lang.Specification {
 	def "hash store 1 changed"() {
 		setup:
 		def logic = new SyncLogic(null)
-		def store1 = Mock(FileHashStore)
-		def store2 = Mock(FileHashStore)
-		store1.isDirty() >> true;
-		store2.isDirty() >> false;
-		def expectedResult = new ArrayList<Action>()
-		def action = new UpdateHashesAction(store1)
-		expectedResult.add(action)
+		def store1 = createMockStore(true)
+		def store2 = createMockStore(false)
+		def expectedResult = createUpdateHashesActions([store1])
 		
 		when:
 		def result = logic.checkHashStores(store1, store2)
@@ -44,13 +38,9 @@ class SyncLogicHashStoreTest extends spock.lang.Specification {
 	def "hash store 2 changed"() {
 		setup:
 		def logic = new SyncLogic(null)
-		def store1 = Mock(FileHashStore)
-		def store2 = Mock(FileHashStore)
-		store1.isDirty() >> false;
-		store2.isDirty() >> true;
-		def expectedResult = new ArrayList<Action>()
-		def action = new UpdateHashesAction(store2)
-		expectedResult.add(action)
+		def store1 = createMockStore(false)
+		def store2 = createMockStore(true)
+		def expectedResult = createUpdateHashesActions([store2])
 		
 		when:
 		def result = logic.checkHashStores(store1, store2)
@@ -62,20 +52,24 @@ class SyncLogicHashStoreTest extends spock.lang.Specification {
 	def "hash both stores changed"() {
 		setup:
 		def logic = new SyncLogic(null)
-		def store1 = Mock(FileHashStore)
-		def store2 = Mock(FileHashStore)
-		store1.isDirty() >> true;
-		store2.isDirty() >> true;
-		def expectedResult = new ArrayList<Action>()
-		def action1 = new UpdateHashesAction(store1)
-		def action2 = new UpdateHashesAction(store2)
-		expectedResult.add(action1)
-		expectedResult.add(action2)
+		def store1 = createMockStore(true)
+		def store2 = createMockStore(true)
+		def expectedResult = createUpdateHashesActions([store1, store2])
 		
 		when:
 		def result = logic.checkHashStores(store1, store2)
 
 		then:
 		expectedResult == result
+	}
+	
+	def private FileHashStore createMockStore(boolean isDirty) {
+		def store = Mock(FileHashStore)
+		store.isDirty() >> isDirty
+		return store
+	}
+	
+	def private ArrayList<Action> createUpdateHashesActions(List<FileHashStore> stores) {
+		return new ArrayList<Action>(stores.collect { new UpdateHashesAction(it) })
 	}
 }
