6120b8e3e68934061c00f159af3271d127538592
Dan Corder <dev@dancorder.com>
2015-01-13 18:54:51 +0000

    Improve integration tests

--- src/integrationTest/groovy/com/dancorder/Archiverify/IntegrationTests/Helpers/Run.groovy
+++ src/integrationTest/groovy/com/dancorder/Archiverify/IntegrationTests/Helpers/Run.groovy
@@ -14,39 +14,19 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package com.dancorder.Archiverify.IntegrationTests
+package com.dancorder.Archiverify.IntegrationTests.Helpers
 
 import java.nio.file.*
 import java.nio.file.attribute.*
 
-class IntegrationTestHelper {
-	public static String runArchiverify() {
+class Run {
+	public static RunResult archiverify() {
 		def command = "java -jar " + getJarFile().toString()
 		
-		def p = Runtime.getRuntime().exec(command)
-		p.waitFor()
+		def process = Runtime.getRuntime().exec(command)
+		process.waitFor()
 		
-		def stdout = inputStreamToString(p.getInputStream())
-		def stderr = inputStreamToString(p.getErrorStream())
-		
-		return stdout
-	}
-	
-	private static String inputStreamToString(InputStream stream) {
-		StringBuilder builder = new StringBuilder();
-		String line;
-		
-		def reader = new BufferedReader(new InputStreamReader(stream))
-		try {
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-				builder.append(System.getProperty("line.separator"))
-			}
-		} finally {
-			reader.close()
-		}
-		
-		return builder.toString()
+		return new RunResult(process)
 	}
 	
 	private static Path getJarFile() {
@@ -83,11 +63,11 @@ class IntegrationTestHelper {
 		
 	private static Path getArchiverifyDirectory() {
 		// Something like /Users/dan/Development/Archiverify/
-		getClassLocation().getParent().getParent().getParent().getParent().getParent().getParent();
+		getClassLocation().getParent().getParent().getParent().getParent().getParent().getParent().getParent();
 	}
 		
 	private static Path getClassLocation() {
-		// Something like /Users/dan/Development/Archiverify/bin/com/dancorder/Archiverify/IntegrationTests/ParameterTests.class
-		Paths.get(ParameterTests.class.getResource("ParameterTests.class").getPath()).toAbsolutePath()
+		// Something like /Users/dan/Development/Archiverify/bin/com/dancorder/Archiverify/IntegrationTests/Helpers/Run.class
+		Paths.get(Run.class.getResource("Run.class").getPath()).toAbsolutePath()
 	}
 }
