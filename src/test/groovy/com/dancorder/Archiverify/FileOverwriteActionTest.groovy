621e2e84c74be1344628d8b8b34419172bbd8dd2
Dan Corder <dev@dancorder.com>
2015-01-16 23:52:52 +0000

    Refactor FileOverwriteActionTest

--- src/test/groovy/com/dancorder/Archiverify/FileOverwriteActionTest.groovy
+++ src/test/groovy/com/dancorder/Archiverify/FileOverwriteActionTest.groovy
@@ -19,30 +19,23 @@ package com.dancorder.Archiverify;
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import com.dancorder.Archiverify.testHelpers.*
 
 public class FileOverwriteActionTest extends spock.lang.Specification {
-	private static final tempDir = Paths.get(System.getProperty("java.io.tmpdir"))
 	private static final generatedHash = "generatedHash"
-	private static FileHashGenerator hashGenerator;
-	private static Path tempFile
-
-	def setup() {
-		tempFile = Files.createTempFile(null, null);
-		hashGenerator = Mock(FileHashGenerator)
-		hashGenerator.calculateMd5(_) >> new String(generatedHash)
-	}
-
-	def cleanup() {
-		Files.delete(tempFile)
-		tempFile = null
-	}
+	private static final tempDir = FileSystem.getTempDirectory()
 
 	def "String value"() {
 		setup:
-		def to = tempDir.resolve("toFile")
-		def foa = new FileOverwriteAction(tempFile, to, new String(generatedHash), hashGenerator)
+		def toFile = tempDir.resolve("toFile")
+		def fromFile = tempDir.resolve("fromFile")
+		
+		def hashGenerator = Mock(FileHashGenerator)
+		hashGenerator.calculateMd5(_) >> new String(generatedHash)
+		
+		def foa = new FileOverwriteAction(fromFile, toFile, new String(generatedHash), hashGenerator)
 
 		expect:
-		foa.toString() == String.format("Overwrite %s with %s with hash %s", to, tempFile, generatedHash)
+		foa.toString() == String.format("Overwrite %s with %s with hash %s", toFile, fromFile, generatedHash)
 	}
 }
