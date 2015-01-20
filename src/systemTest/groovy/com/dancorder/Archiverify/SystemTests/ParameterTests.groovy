d18e97fd26f04cfb271b0b3dfcab07362897879c
Dan Corder <dev@dancorder.com>
2015-01-20 20:44:24 +0000

    Rename integration tests to system tests

--- src/systemTest/groovy/com/dancorder/Archiverify/SystemTests/ParameterTests.groovy
+++ src/systemTest/groovy/com/dancorder/Archiverify/SystemTests/ParameterTests.groovy
@@ -14,7 +14,7 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package com.dancorder.Archiverify.IntegrationTests;
+package com.dancorder.Archiverify.SystemTests;
 
 import com.dancorder.Archiverify.testHelpers.*
 import java.nio.file.Path
